5938dd4cee999e92de323d72f80a95a7203151fd
andyhall <andy@fenomas.com>
2022-07-11 17:31:21 +0900

    Add explicity config files

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "root": true,
+    "extends": [
+        "eslint:recommended",
+    ],
+    plugins: [],
+    "env": {
+        "node": true,
+        "browser": true,
+        "es6": true,
+    },
+    "parserOptions": {
+        "ecmaVersion": 10,
+        "sourceType": "module",
+    },
+    "rules": {
+        "strict": ["error", "global"],
+
+        "no-unused-vars": ["warn", { "args": "none" }],
+        "no-empty": "off",
+        "no-console": "off",
+        "no-return-await": "error",
+
+        "semi": ["error", "never"],
+        "no-unexpected-multiline": "error",
+
+    },
+}
